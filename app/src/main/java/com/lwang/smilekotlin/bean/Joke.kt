7b72f5c31232ca7a314841dac4cbf13267972933
wanglu <lwang057@163.com>
2017-12-10 22:47:21 +0800

    搭建内涵段子子页面

--- app/src/main/java/com/lwang/smilekotlin/bean/Joke.kt
+++ app/src/main/java/com/lwang/smilekotlin/bean/Joke.kt
@@ -4,3 +4,6 @@ package com.lwang.smilekotlin.bean
  * Created by lwang on 17-12-9.
  */
 data class Joke(val title: String, val text: String)
+
+
+data class Rhesis(val title: String, val text: String)
